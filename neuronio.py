f88450fa66736e7a9e29662a878382398e6c78ea
yaminyassin <yamin20088@live.com>
2020-10-25 21:26:31 +0000

    initial

---
+++ neuronio.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+
+
+class neuronio:
+    def __init__(self, weights, bias=1, lr=0.15) -> None:
+        self.weights = weights
+        self.bias = bias
+        self.lr = lr
+
+
+
+        
